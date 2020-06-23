362306f33d098c97daecca1bff786d36a4ff6727
Takayuki Murooka <takayuki5168@gmail.com>
2020-06-24 03:29:00 +0900

    add add_ground_object.cpp

---
+++ src/add_ground_object.cpp
@@ -0,0 +1,60 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+// MoveIt!
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/GetStateValidity.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/ApplyPlanningScene.h>
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "planning_scene_ros_api_tutorial");
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  ros::NodeHandle node_handle;
+
+  ros::Publisher planning_scene_diff_publisher = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+  ros::WallDuration sleep_t(0.5);
+  while (planning_scene_diff_publisher.getNumSubscribers() < 1)
+  {
+    sleep_t.sleep();
+  }
+
+  moveit_msgs::AttachedCollisionObject attached_object;
+  attached_object.link_name = "ground_object";
+  attached_object.object.header.frame_id = "world";
+  attached_object.object.id = "box";
+
+  geometry_msgs::Pose pose;
+  pose.position.z = -0.5;
+  pose.orientation.w = 1.0;
+
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 10;
+  primitive.dimensions[1] = 10;
+  primitive.dimensions[2] = 1;
+
+  attached_object.object.primitives.push_back(primitive);
+  attached_object.object.primitive_poses.push_back(pose);
+
+  attached_object.object.operation = attached_object.object.ADD;
+
+  moveit_msgs::PlanningScene planning_scene;
+  planning_scene.world.collision_objects.push_back(attached_object.object);
+  planning_scene.is_diff = true;
+  planning_scene_diff_publisher.publish(planning_scene);
+
+  ros::shutdown();
+  return 0;
+}
